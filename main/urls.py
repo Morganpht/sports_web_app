89a9a906e969a1b6783e63df75075ece02c2bdbf
Morgan Thomas <MPHThomas@outlook.com>
2021-08-11 12:09:56 +0100

    Completed user sign up and login/logout functionality. Styling, third party login and password reset still required.

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.sign_in, name="sign-in"),
+    path("", views.homepage, name="homepage"),
     path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
 ]
